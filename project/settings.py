3abe426911d61c82fe1deb373d76df2b0cfa2c00
Umair Khan <umair.waheed@gmail.com>
2024-02-20 13:02:55 +0500

    Format

--- project/settings.py
+++ project/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
     }
 }
 
-#AUTH_USER_MODEL = "core.User"
+# AUTH_USER_MODEL = "core.User"
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
