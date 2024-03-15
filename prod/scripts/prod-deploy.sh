f033a639b2815f701134f0e22738d4acb0668dab
Umair Khan <umair.waheed@gmail.com>
2024-03-15 12:10:43 +0500

    Update prod script

--- prod/scripts/prod-deploy.sh
+++ prod/scripts/prod-deploy.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-docker-compose build
-docker-compose up -d
+docker-compose -f docker-compose.prod.yml build
+docker-compose -f docker-compose.prod.yml up -d
+docker-compose -f docker-compose.prod.yml exec web ./manage.py migrate
+docker-compose -f docker-compose.prod.yml exec web ./manage.py collectstatic --no-input
